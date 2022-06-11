83066ead0f76273b58b71070c34988c81e25c8a5
Josphat Otieno <josphathumphrey5@gmail.com>
2022-06-11 12:03:19 +0300

    modified file

---
+++ main.js
@@ -0,0 +1,31 @@
+const typeOfActivity = document.querySelector('select');
+const btn = document.querySelector('.btn');
+const activityModal = document.querySelector('#activity-modal');
+const activityWrapper = document.querySelector('#activity-wrapper');
+
+btn.addEventListener('click', getActivity);
+
+async function getActivity(e) {
+    e.preventDefault();
+
+    const type = typeOfActivity.options[typeOfActivity.selectedIndex].text;
+
+    console.log(type)
+    const response = await fetch(`http://www.boredapi.com/api/activity?type=${type.toLowerCase()}`);
+
+    const data = await response.json();
+
+    displayActivity(data.activity);
+    console.log(data.activity);
+    
+}
+
+function displayActivity(activity) {
+    activityModal.style.display = 'flex';
+
+    activityWrapper.innerHTML = `
+        <p>There you go! <i class="fa fa-rocket" aria-hidden="true"></i> It's time to <span class='activity'>${activity}<span>.</p>
+    `
+}
+
+window.addEventListener('click', e => e.target == activityModal ? activityModal.style.display = 'none' : false)
